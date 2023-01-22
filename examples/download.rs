6832fde9ed852c1bda6dbf2f1ebd94eaf2c4a3db
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-22 13:00:03 +0100

    Download the tokenizer too.

--- examples/download.rs
+++ examples/download.rs
@@ -11,5 +11,11 @@ async fn main() -> Result<(), Gpt2Error> {
         println!("Downloading {url:?} into {filename:?}");
         download(url, filename, max_files, chunk_size).await?;
     }
+    let filename = "tokenizer.json";
+    if !std::path::Path::new(filename).exists() {
+        let url = "https://huggingface.co/gpt2/resolve/main/tokenizer.json";
+        println!("Downloading {url:?} into {filename:?}");
+        download(url, filename, max_files, chunk_size).await?;
+    }
     Ok(())
 }
