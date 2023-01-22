5beac4a99ca7d35ab693366ab8602d041f0c0298
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-22 13:03:09 +0100

    Fix example.

--- examples/download.rs
+++ examples/download.rs
@@ -3,10 +3,10 @@ use fast_gpt2::Gpt2Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Gpt2Error> {
+    let max_files = 100;
+    let chunk_size = 10_000_000;
     let filename = "model.safetensors";
     if !std::path::Path::new(filename).exists() {
-        let max_files = 100;
-        let chunk_size = 10_000_000;
         let url = "https://huggingface.co/Narsil/gpt2/resolve/main/model.safetensors";
         println!("Downloading {url:?} into {filename:?}");
         download(url, filename, max_files, chunk_size).await?;
