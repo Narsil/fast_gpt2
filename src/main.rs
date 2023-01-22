22a100d41074df3f27641e701236fa7049a9a5a7
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-22 21:22:10 +0100

    Clippy.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::net::SocketAddr;
 use tokenizers::Tokenizer;
 use tower_http::trace::{self, TraceLayer};
-use tracing::{span, Level};
+use tracing::{instrument, Level};
 
 #[derive(Clone)]
 struct AppState {
@@ -20,15 +20,8 @@ fn leak_buffer(buffer: Mmap) -> &'static [u8] {
     buffer
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Gpt2Error> {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-    let span = span!(Level::TRACE, "loading");
-    let _enter = span.enter();
-
-    let start = std::time::Instant::now();
-    let filename = "model.safetensors";
+#[instrument]
+async fn get_model(filename: &str) -> Result<Gpt2, Gpt2Error> {
     let max_files = 100;
     let chunk_size = 10_000_000;
     if !std::path::Path::new(filename).exists() {
@@ -41,23 +34,33 @@ async fn main() -> Result<(), Gpt2Error> {
     let buffer: &'static [u8] = leak_buffer(buffer);
     let tensors: SafeTensors<'static> = SafeTensors::deserialize(buffer)?;
     let tensors: &'static SafeTensors<'static> = Box::leak(Box::new(tensors));
-    println!("Safetensors {:?}", start.elapsed());
+    let num_heads = 12;
+    Ok(Gpt2::from_tensors(tensors, num_heads))
+}
 
-    let filename = "tokenizer.json";
+#[instrument]
+async fn get_tokenizer(filename: &str) -> Result<Tokenizer, Gpt2Error> {
+    let max_files = 100;
+    let chunk_size = 10_000_000;
     if !std::path::Path::new(filename).exists() {
         let url = "https://huggingface.co/gpt2/resolve/main/tokenizer.json";
         println!("Downloading {url:?} into {filename:?}");
         download(url, filename, max_files, chunk_size).await?;
     }
-    let tokenizer = Tokenizer::from_file(filename).unwrap();
-    println!("Tokenizer {:?}", start.elapsed());
+    Ok(Tokenizer::from_file(filename).unwrap())
+}
 
-    let num_heads = 12;
-    let gpt2 = Gpt2::from_tensors(tensors, num_heads);
-    let state = AppState {
-        model: gpt2,
-        tokenizer,
-    };
+#[tokio::main]
+async fn main() -> Result<(), Gpt2Error> {
+    // initialize tracing
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "fast_gpt2=debug,tower_http=debug")
+    }
+    tracing_subscriber::fmt::init();
+    let model = get_model("model.safetensors").await?;
+    let tokenizer = get_tokenizer("tokenizer.json").await?;
+
+    let state = AppState { model, tokenizer };
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
@@ -97,25 +100,17 @@ struct Outputs {
 async fn inference(
     (State(state), Json(payload)): (State<AppState>, Json<Inputs>),
 ) -> impl IntoResponse {
-    let span = span!(Level::TRACE, "inference request");
-    let _enter = span.enter();
     let tokenizer = state.tokenizer;
     let encoded = tokenizer.encode(payload.inputs, false).unwrap();
     let mut ids = encoded.get_ids().to_vec();
     let mut past_key_values = state.model.empty_past_key_values();
     let mut current_ids = ids.clone();
     for _i in 0..20 {
-        // let start = std::time::Instant::now();
-        let span = span!(Level::TRACE, "inference loop");
-        let _enter = span.enter();
         let logits = state.model.forward(&current_ids, &mut past_key_values);
         let new_id = special_argmax(&logits);
         ids.push(new_id as u32);
         current_ids = vec![new_id as u32];
-        // println!("Loop in {:?}", start.elapsed());
     }
-    // println!("Result {:?}", tokenizer.decode(ids, false));
-    // println!("Total Inference {:?}", start.elapsed());
     let generated_text = tokenizer.decode(ids, false).unwrap();
     let output = Outputs { generated_text };
     Json(output)
