c58f81fe22df1ddcedc501cb4d92330840bd419a
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-23 17:59:41 +0100

    More detailed comparison.

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import datetime
-from transformers import pipeline
 
 start = datetime.datetime.now()
+import torch
+
+print(f"Loaded torch {datetime.datetime.now() - start}")
+from transformers import pipeline
+
+print(f"Loaded transformers {datetime.datetime.now() - start}")
+
+
 pipe = pipeline(task="text-generation", model="gpt2", do_sample=False)
 pipe.model.config.max_length = None
 print(f"Loaded in {datetime.datetime.now() - start}")
 inf_start = datetime.datetime.now()
 new_tokens = 10
 out = pipe("My name is", max_length=3 + new_tokens)
-print(f"Ran in {(datetime.datetime.now() - start)}")
 print(f"Tokens: {(datetime.datetime.now() - inf_start)/new_tokens}/tokens")
 print(f"Inference took: {(datetime.datetime.now() - inf_start)}")
 print(out)
+print(f"Ran in {(datetime.datetime.now() - start)}")
